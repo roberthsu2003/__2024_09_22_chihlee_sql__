437515e3d7d4917da9ff361831afe5de30d8215d
roberthsu2003 <roberthsu2003@gmail.com>
2024-09-29 16:05:27 +0800

    modify

--- sql/lesson2_2.sql
+++ sql/lesson2_2.sql
@@ -9,3 +9,15 @@ CREATE TABLE IF NOT EXISTS hospital (
 	contact VARCHAR(7),
 	address VARCHAR(50)
 );
+
+/*城市,啟始時間,結束時間,最高溫度,最低溫度,感覺*/
+
+CREATE TABLE IF NOT EXISTS weather(
+    id SERIAL PRIMARY KEY,
+	city VARCHAR(20) NOT NULL,
+    startDate timestamp,
+	endDate timestamp,
+	hight real,
+	low real,
+	status VARCHAR(20)
+);
