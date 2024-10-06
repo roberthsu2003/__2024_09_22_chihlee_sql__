5dab8d5c6eff9d89fe55adf87e6286daa506d71d
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-06 15:02:40 +0800

    modify

--- sql/lesson3_4.sql
+++ sql/lesson3_4.sql
@@ -18,3 +18,12 @@ VALUES('生物','小黑');
 INSERT INTO student(major,name)
 VALUES(NULL,'小藍');
 
+/*新增多筆*/
+INSERT INTO student(major,name)
+VALUES('國文','小線'),('數學','小綠');
+
+/*傳回新增的資訊*/
+INSERT INTO student(major,name)
+VALUES('生物','小線'),('歷史','小綠')
+RETURNING *;
+
