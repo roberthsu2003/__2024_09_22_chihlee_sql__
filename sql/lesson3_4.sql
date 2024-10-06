43e0893d112e603cd7502b060b2314610e81b466
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-06 14:54:45 +0800

    modify

--- sql/lesson3_4.sql
+++ sql/lesson3_4.sql
@@ -4,3 +4,17 @@ CREATE TABLE student(
 	major VARCHAR(20),
 	PRIMARY KEY(student_id)
 );
+
+SELECT *
+FROM student;
+
+/*新增資料*/
+INSERT INTO student(name,major)
+VALUES('小白','歷史');
+
+INSERT INTO student(major,name)
+VALUES('生物','小黑');
+
+INSERT INTO student(major,name)
+VALUES(NULL,'小藍');
+
