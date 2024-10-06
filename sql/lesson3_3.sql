8d4d20b548a6da5f9e1da1f9bcd0f6afede6741f
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-06 14:27:13 +0800

    modify

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -61,4 +61,17 @@ FROM stations
 WHERE id NOT BETWEEN 10 AND 20;
 
 
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+ORDER BY id DESC;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+ORDER BY code DESC; /*數字字串排序*/
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+ORDER BY e_name; /*英文字排序*/
+
+
 
