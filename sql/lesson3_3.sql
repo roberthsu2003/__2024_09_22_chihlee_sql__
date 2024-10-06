f7f1269d7987b5d91b056f99cc21749a8ed355fa
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-06 11:53:00 +0800

    lesson03_03

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -1,15 +1,37 @@
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
-WHERE code = '1001'
+WHERE code = '1001';
 
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
-WHERE code = '1001' OR code = '1002'
+WHERE code = '1001' OR code = '1002';
 
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
-WHERE code IN ('1001','1002')
+WHERE code IN ('1001','1002');
 
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
-WHERE code = '1001' OR name = '八堵'
+WHERE code = '1001' OR name = '八堵';
+
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name = '基隆';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations;
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台_';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台%';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name LIKE '%港';
+
+
