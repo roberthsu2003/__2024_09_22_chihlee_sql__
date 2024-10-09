e07cb6bc7f16a7dd433e1aab7d7778f286e097af
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-09 13:53:01 +0800

    Update test.sql

--- sql/test.sql
+++ sql/test.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS stations(
 	id Serial PRIMARY KEY,
-	stationCode VARCHAR(5),
+	stationCode VARCHAR(5) UNIQUE NOTNULL, /*一定要,因為是foreign的parent*/
 	stationName VARCHAR(20) NOT NULL,
 	name VARCHAR(20),
 	stationAddrTw VARCHAR(50),
@@ -19,7 +19,11 @@ CREATE TABLE IF NOT EXISTS station_in_out(
 	staCode VARCHAR(5) NOT NULL,
 	gateInComingCnt INTEGER,
 	gateOutGoingCnt INTEGER,
-	PRIMARY KEY (date,staCode)
+	PRIMARY KEY (date,staCode),
+	FOREIGN KEY (staCode) /*可設可不設,有設可以保持資料的完整性*/
+	REFERENCES stations(stationcode)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS station_in_out; 
