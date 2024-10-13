9ffcb742ea1cb955f5229cb8116b405d24ee0b4f
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-13 14:17:12 +0800

    lesson4_07

--- sql/lesson4_3.sql
+++ sql/lesson4_3.sql
@@ -20,3 +20,13 @@ SELECT customer_id,
 FROM payment
 GROUP BY customer_id
 ORDER BY COUNT(amount) ASC;
+
+SELECT customer_id,
+		SUM(amount) AS 加總,
+		AVG(amount) AS 平均數量,
+		COUNT(amount) AS 筆數,
+		MAX(amount) AS 最大,
+		MIN(amount) AS 最小
+FROM payment
+GROUP BY customer_id
+ORDER BY 筆數 ASC;
