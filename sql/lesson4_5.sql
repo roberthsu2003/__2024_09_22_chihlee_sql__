04a6f5af6b23296481f1608f1ba29e1e95d65f5c
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-13 16:06:10 +0800

    lesson4_11

--- sql/lesson4_5.sql
+++ sql/lesson4_5.sql
@@ -11,4 +11,29 @@ HAVING SUM(amount) > 200;
 
 SELECT store_id ,COUNT(customer_id) AS 數量
 FROM customer
-GROUP BY store_id;
+GROUP BY store_id
+HAVING COUNT(customer_id) > 300;
+
+/*subquery*/
+SELECT *
+FROM country;
+
+SELECT country_id
+FROM country
+WHERE country='Taiwan';
+
+SELECT city
+FROM city
+WHERE country_id = 92;
+
+SELECT city
+FROM city
+WHERE country_id = (
+	SELECT country_id
+	FROM country
+	WHERE country='Taiwan'
+);
+
+SELECT city
+FROM city JOIN country ON city.country_id = country.country_id
+WHERE country = 'Taiwan';
