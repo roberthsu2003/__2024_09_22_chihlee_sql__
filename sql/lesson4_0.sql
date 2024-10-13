8955e6c4959661b2ca409f925651c614e60481cf
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-13 09:43:45 +0800

    lesson4_01

---
+++ sql/lesson4_0.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM customer;
+
+SELECT *
+FROM customer
+WHERE first_name='Jamie';
+
+SELECT *
+FROM customer
+WHERE first_name='Jamie' and last_name='Rice';
+
+SELECT *
+FROM customer
+WHERE last_name='Rodriquez' or first_name='Adam';
+
+SELECT *
+FROM customer
+WHERE first_name='Ann' or first_name='Anne' or first_name='Annie';
+
+SELECT *
+FROM customer
+WHERE first_name like 'Ann%';
+
+SELECT *
+FROM customer
+WHERE first_name like 'A%' and length(first_name)>=3 and length(first_name)<=5;
+
+
+SELECT *
+FROM customer
+WHERE first_name like 'Bra%' and last_name!='Motley';
