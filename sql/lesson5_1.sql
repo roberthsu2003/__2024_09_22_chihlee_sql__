4ef1b501bea80493f07fe16cebc4ca0f080c243e
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-20 11:06:59 +0800

    lesson5_7

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -37,6 +37,16 @@ WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname='基隆'
 GROUP BY 站名,日期
 ORDER BY SUM(gateincomingcnt) DESC;
 
+/*基隆火車站2020,2021,2022,每年進站人數*/
+SELECT stationname AS 站名, EXTRACT(YEAR FROM date) as 年份,SUM(gateincomingcnt) AS 當年總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE EXTRACT(YEAR FROM date) IN (2020,2021,2022) AND stationname='基隆'
+GROUP BY 站名,年份;
+
+
+
+
+
 
 
 
