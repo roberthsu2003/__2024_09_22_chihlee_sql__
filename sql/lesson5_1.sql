9fe157eb528d25ca71b6856808adb63dbae57ce9
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-20 10:11:37 +0800

    lesson5_2

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -4,9 +4,11 @@ FROM stations;
 SELECT *
 FROM station_in_out;
 /*全省各站點2022年進站總人數*/
-SELECT stationname as 站名
+SELECT stationname AS 站名, SUM(gateincomingcnt) AS 總進站人數
 FROM stations JOIN station_in_out ON stationcode = stacode
 /*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
-WHERE EXTRACT(YEAR FROM date) = 2022;
+WHERE EXTRACT(YEAR FROM date) = 2022
+GROUP BY 站名
+ORDER BY 總進站人數 DESC;
 
 
