3142b6064489945e4ab7e1b6f09f2ed34059071f
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-20 11:54:06 +0800

    modify

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -61,6 +61,19 @@ WHERE gateincomingcnt = (
 );
 
 
+/*各站點進站人數最多的一筆*/
+
+SELECT name,date,gateincomingcnt
+FROM stations JOIN station_in_out ON stationcode = stacode
+WHERE (stationname,gateincomingcnt) IN (
+	SELECT stationname AS 站名,MAX(gateincomingcnt) AS 最多人數
+	FROM stations JOIN station_in_out ON stationcode = stacode
+	GROUP BY 站名	
+)
+ORDER BY gateincomingcnt;
 
 
+SELECT stationname AS 站名,MAX(gateincomingcnt) AS 最多人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+GROUP BY 站名;
 
