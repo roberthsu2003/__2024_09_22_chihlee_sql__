c9e74ddd7fc3a48ff4222f0ec16eca665b451671
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-20 10:39:47 +0800

    lesson5_5

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -23,10 +23,11 @@ ORDER BY 總進站人數 DESC;
 
 
 /*基隆火車站2020年,每月份進站人數*/
-SELECT stationname AS 站名, date AS 日期, gateincomingcnt AS 每日進站人數
+SELECT stationname AS 站名, TO_CHAR(date, 'YYYY-MM') AS 日期,SUM(gateincomingcnt) AS 當月總進站人數
 FROM stations JOIN station_in_out ON stationcode = stacode
 /*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
-WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname='基隆';
+WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname='基隆'
+GROUP BY 站名,日期;
 
 
 
