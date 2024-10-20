18bbcf0701dc674589dee327f3e7067371132a36
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-20 10:16:52 +0800

    lesson5_3

--- sql/lesson5_1.sql
+++ sql/lesson5_1.sql
@@ -12,3 +12,13 @@ GROUP BY 站名
 ORDER BY 總進站人數 DESC;
 
 
+/*全省各站點2022年進站總人數大於5佰萬人的站點*/
+SELECT stationname AS 站名, SUM(gateincomingcnt) AS 總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+/*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
+WHERE EXTRACT(YEAR FROM date) = 2022
+GROUP BY 站名
+HAVING SUM(gateincomingcnt) >= 5000000
+ORDER BY 總進站人數 DESC;
+
+
