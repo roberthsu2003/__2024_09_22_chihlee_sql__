663e1f4d2f0ddc156eee6174c3cecb771334cfe7
roberthsu2003 <roberthsu2003@gmail.com>
2024-10-27 14:34:23 +0800

    lesson6_10

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
-st.write("# Hello! Streamlit!")
+import psycopg2
+conn = psycopg2.connect(
+    database="mydatabase_a31",
+    user="a31",
+    password="raspberry",
+    host="piRobert0301.local",  # 通常是 '127.0.0.1' 或 'localhost'
+    port="5432")
+cur = conn.cursor()
+cur.execute("SELECT * FROM stations;")
+rows = cur.fetchall()
+names = []
+for row in rows:
+    names.append(row[2])
+    
+st.write(names)
