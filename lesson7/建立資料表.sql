775ebd63136be500469f3fdf757239fca63f4a5e
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-03 10:24:47 +0800

    modify

---
+++ lesson7/建立資料表.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS 市場(
+	name VARCHAR(20),
+	country VARCHAR(20),
+	PRIMARY KEY(name)
+);
+
+CREATE TABLE IF NOT EXISTS 股市(
+	stock_id SERIAL,
+	date DATE,
+	open NUMERIC(17,10),
+	high NUMERIC(17,10),
+	low NUMERIC(17,10),
+	close NUMERIC(17,10),
+	adj_close NUMERIC(17,10),
+	volume BIGINT DEFAULT 0,
+	name VARCHAR(20),
+	PRIMARY KEY(stock_id),
+	FOREIGN KEY(name) REFERENCES 市場(name) 
+	ON DELETE NO ACTION
+	ON UPDATE CASCADE
+);
