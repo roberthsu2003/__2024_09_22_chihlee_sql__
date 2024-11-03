fbf3e4df65a5d8740f6c39b969004b50b7ab2fed
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-03 13:58:24 +0800

    modify

--- lesson7/建立資料表.sql
+++ lesson7/建立資料表.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS 市場(
 	name VARCHAR(20),
 	country VARCHAR(20),
-	PRIMARY KEY(name)
+	PRIMARY KEY(name),
+	UNIQUE(name,country) /*不可以有重覆的資料*/
 );
 
 CREATE TABLE IF NOT EXISTS 股市(
@@ -14,8 +15,9 @@ CREATE TABLE IF NOT EXISTS 股市(
 	adj_close NUMERIC(17,10),
 	volume BIGINT DEFAULT 0,
 	name VARCHAR(20),
-	PRIMARY KEY(stock_id),
-	FOREIGN KEY(name) REFERENCES 市場(name) 
+	PRIMARY KEY(stock_id),	
+	UNIQUE(date,name),
+	FOREIGN KEY(name) REFERENCES 市場(name)
 	ON DELETE NO ACTION
 	ON UPDATE CASCADE
 );
