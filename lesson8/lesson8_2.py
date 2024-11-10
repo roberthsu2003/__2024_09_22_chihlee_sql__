2d264cf1635229511d1f8b0e22089afc483de6ea
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-10 14:56:39 +0800

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -40,31 +40,29 @@ def get_country():
     conn.close()
     input_dict = dict(all_country)
     return input_dict.values()
-
-def user_select():
-    print('使用者選擇了')
-    global df
-    selectedCountry:tuple[str] = tuple(st.session_state.stocks) 
-    if selectedCountry: 
-        st.title('世界大盤分析')  
-        df = pd.DataFrame(getData(selectedCountry),columns=['國家','代號','日期','收盤價','成交量'])
-        df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
-        st.line_chart(data=df,x='日期',y='收盤價',color='國家')
+   
     
 
 
 default_country = '台灣'
 with st.sidebar:    
     st.title('請選擇股票市場:')    
-    st.multiselect("請選擇",get_country(),
-                    default=default_country,
-                    placeholder="請選擇市場",
-                    label_visibility='hidden',
-                    key='stocks',
-                    on_change=user_select)
+    options:list[str] =st.multiselect("請選擇",get_country(),
+                        default=default_country,
+                        placeholder="請選擇市場",
+                        label_visibility='hidden',
+                        key='stocks'
+                        )
+    
+selectedCountry:tuple[str] = tuple(options) 
+if selectedCountry: 
+    st.title('世界大盤分析')
+    df = pd.DataFrame(getData(selectedCountry),columns=['國家','代號','日期','收盤價','成交量'])
+    df['收盤價'] = df['收盤價'].astype('float').round(decimals=2)
+    st.line_chart(data=df,x='日期',y='收盤價',color='國家')
     
 
-user_select()
+
 
 
 
