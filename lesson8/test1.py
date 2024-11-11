8c1c93201053910fbfee586a79621449d54802f2
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-11 10:54:46 +0800

    modify

---
+++ lesson8/test1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
