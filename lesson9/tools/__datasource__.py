68748f3e1c6818266dd85f966db36d1f20db81e1
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-17 14:36:50 +0800

    modify

--- lesson9/tools/__datasource__.py
+++ lesson9/tools/__datasource__.py
@@ -1,4 +1,7 @@
 import requests
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 
 #常數
@@ -23,4 +26,35 @@ def get_data()->list | None:
     
 def save_to_database(data:list[dict])->None:
     print(data)
-    print("data的資料要放進資料庫")
+    #建立資料表
+    load_dotenv()
+    conn = psycopg2.connect(host=os.environ['POSTGRE_HOST'],database=os.environ['POSTGRE_DATABASE'],user=os.environ['POSTGRE_USER'],password=os.environ['POSTGRE_PASSWORD'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql = '''
+            CREATE TABLE IF NOT EXISTS 站點資訊(
+            站點編號 VARCHAR(10),
+            站點名稱 VARCHAR(30) NOT NULL,
+            行政區 VARCHAR(10) NOT NULL,
+            站點地址 VARCHAR(50),
+            lat NUMERIC(15,11),
+            lng NUMERIC(15,11),
+            PRIMARY KEY(站點編號)
+            );
+
+            CREATE TABLE IF NOT EXISTS youbike(
+            日期 TIMESTAMP,
+            編號 VARCHAR(10),
+            總車輛 INTEGER,
+            可借 INTEGER,
+            可還 INTEGER,
+            活動 BOOLEAN,
+            PRIMARY KEY(日期,編號),
+            FOREIGN KEY(編號) REFERENCES 站點資訊(站點編號) 
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+            );
+            '''
+            cursor.execute(sql)
+            
+    conn.close()
