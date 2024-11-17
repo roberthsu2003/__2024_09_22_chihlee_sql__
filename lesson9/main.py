4a9ad7f59dbc89646583e64b878cb6578743d16e
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-17 10:13:53 +0800

    modify

--- lesson9/main.py
+++ lesson9/main.py
@@ -1,2 +1,17 @@
+
 from dotenv import load_dotenv
+import streamlit as st
+from time import sleep
+
 load_dotenv()
+placeholder = st.empty()
+with placeholder:
+    with st.spinner('下載資料中...'):
+        sleep(5)
+
+    st.success("下載完成",icon=":material/favorite:")
+    sleep(3)
+
+
+placeholder.empty()
+    
