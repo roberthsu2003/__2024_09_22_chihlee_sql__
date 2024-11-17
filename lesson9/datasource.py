b1cc5e43dbe52329942ef323857747e136c1a732
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-17 10:26:40 +0800

    lesson9_02

---
+++ lesson9/datasource.py
@@ -0,0 +1,11 @@
+#常數
+PI = 3.1415926
+
+#自訂的class
+class CustomClass:
+    pass
+
+import time
+#自訂的function
+def get_data():
+    time.sleep(5)
--- lesson9/main.py
+++ lesson9/main.py
@@ -2,12 +2,13 @@
 from dotenv import load_dotenv
 import streamlit as st
 from time import sleep
-
+from datasource import get_data
 load_dotenv()
+
 placeholder = st.empty()
 with placeholder:
     with st.spinner('下載資料中...'):
-        sleep(5)
+        get_data()
 
     st.success("下載完成",icon=":material/favorite:")
     sleep(3)
