585b659cb8c82419e6ed73e751136b3dc472ea6a
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-17 11:59:52 +0800

    modify

--- lesson9/tools/__datasource__.py
+++ lesson9/tools/__datasource__.py
@@ -1,3 +1,4 @@
+import requests
 #常數
 PI = 3.1415926
 
@@ -10,3 +11,5 @@ import time
 def get_data():
     time.sleep(5)
     url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json' 
+    response = requests.get(url)
+    type(response)
